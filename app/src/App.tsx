2e964b074243ad9443b837cae2bb55685de03dc6
Yasir Nadeem <m-yasir.dev@protonmail.com>
2021-07-22 06:13:43 +0500

    App: Add Evergreen UI module. Implement UI. Integrate Mock APIs.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// React
+import { useEffect, useState } from "react";
+// UI
+import { Checkbox, Heading, majorScale, Pane, SortIcon, Text } from "evergreen-ui";
+
+// Util
+import moment, { Moment } from "moment";
+import { MOMENT_DATE_FORMAT, MOMENT_TIME_FORMAT } from "./constants";
+import { getTasks } from "./api";
+import { Task } from "./types";
+
+window.moment = moment;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [dateTime, setDateTime] = useState<Moment>(moment());
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDateTime(moment());
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<Pane>
+			<Pane display="flex" justifyContent="space-between" marginBottom={majorScale(5)}>
+        {/* Date Time and Sort Icon Header */}
+				<Pane display="flex">
+					<Heading fontWeight={800} size={800}>
+						Today
+					</Heading>
+					<Text alignSelf="flex-end" marginLeft={10}>
+						{dateTime.format(MOMENT_DATE_FORMAT)}{" "}
+						{dateTime.format(MOMENT_TIME_FORMAT)}
+					</Text>
+				</Pane>
+        <Pane>
+          <SortIcon color="gray700" />
+        </Pane>
+			</Pane>
+      {/* <HorizontalRule /> */}
+      <Tasks />
+		</Pane>
+	);
 }
+
+const Tasks = (props = {}) => {
+	const [tasks, setTasks] = useState<Task[]>([]);
+	
+	useEffect(() => {
+		const initTasks = async () => {
+			try {
+				const tasks = await getTasks();
+				setTasks(tasks);
+			} catch (e) {
+				console.trace(e);
+			}
+		};
+		initTasks();
+	}, []);
+	
+	const handleTaskComplete = (idx: number) => () => {
+		setTasks((currentTasks) => {
+			const newTasks = currentTasks.slice();
+			newTasks[idx].completed = !newTasks[idx].completed;
+			
+			return newTasks;
+		})
+	};
+
+	return (
+		<Pane>
+			<Heading fontWeight={600} fontSize={majorScale(2)}>
+				Tasks
+			</Heading>
+			<HorizontalRule />
+			<Pane display="flex" flexDirection="column">
+				{tasks.map((task, idx) => (
+					<Pane key={idx}>
+						<Pane alignItems="center" display="flex">
+							<Checkbox checked={task.completed} onChange={handleTaskComplete(idx)} />
+							<Text
+								marginLeft={majorScale(1)}
+								textDecoration={
+									task.completed ? "line-through" : "none"
+								}
+							>
+								{task.content}
+							</Text>
+						</Pane>
+					</Pane>
+				))}
+			</Pane>
+		</Pane>
+	);
+}
+
+const HorizontalRule = () => (
+	<Pane
+		borderColor="#eee"
+		borderStyle="solid"
+		marginY={majorScale(2)}
+		border="0"
+		borderTopWidth={1}
+		height={1}
+	/>
+);
 
 export default App;
---
+++ app/src/types.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id: number;
+    content: string;
+    completed: boolean;
+    createdAt: Date;
+}

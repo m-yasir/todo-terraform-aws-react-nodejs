c24e13778276e9d775832124c3a2823f595b5b47
Yasir Nadeem <m-yasir.dev@protonmail.com>
2021-07-23 03:41:42 +0500

    Server: Implement Todo Endpoints.

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,8 +1,70 @@
-const router = require('express').Router();
+const router    = require("express").Router();
+const { Todo }  = require("../models");
 
-router.get('/', (req, res) => {
-    res.status(200);
-    res.send([]);
-})
+// Break into controller and routes if needed for cleaner code
+router.get("/", async (req, res) => {
+	try {
+		const todos = await Todo.findAll();
+		res.status(200);
+		res.send(todos);
+	} catch (err) {
+		res.status(404);
+		res.send(err);
+	}
+});
+
+router.post("/", async (req, res) => {
+	try {
+		const { content, completed } = req.body;
+		if (req.body.content == null || req.body.completed == null) {
+			throw new Error("Body is missing details.");
+		}
+		const todos = await Todo.create({
+			content,
+			completed
+		});
+		res.status(200);
+		res.send(todos);
+	} catch (err) {
+		res.status(400);
+		res.send(err);
+	}
+});
+
+router.patch("/:id", async (req, res) => {
+	try {
+		const { content, completed } = req.body;
+		if (req.body.content == null || req.body.completed == null) {
+			throw new Error("Body is missing details.");
+		}
+		const todos = await Todo.update(
+			{
+				content,
+				completed
+			},
+			{ where: { id: req.params.id } }
+		);
+		res.status(200);
+		res.send({ id: req.params.id });
+	} catch (err) {
+		res.status(400);
+		res.send(err);
+	}
+});
+
+router.delete("/:id", async (req, res) => {
+	try {
+		const { content, completed } = req.body;
+		if (req.body.content == null || req.body.completed == null) {
+			throw new Error("Body is missing details.");
+		}
+		const todos = await Todo.destroy({ where: { id: req.params.id } });
+		res.status(200);
+		res.send({ id: req.params.id });
+	} catch (err) {
+		res.status(400);
+		res.send(err);
+	}
+});
 
 module.exports = router;
